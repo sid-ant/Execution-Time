fd9c36533ab409faa665832f6f04544c10a5172c
Siddhant Chhabra <siddhant1042@gmail.com>
2019-06-30 21:14:06 +0530

    Minor Changes

--- execution_time.py
+++ execution_time.py
@@ -2,17 +2,17 @@ import functools
 import time
 import os
 import logging
-from inspect import getmembers,isfunction
+from inspect import getmembers,isfunction,ismethod,isclass
 import sys
 
 
-class Timer:
+class ExecutionTime:
 
     logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
     rootLogger = logging.getLogger()
     rootLogger.setLevel(logging.DEBUG)
 
-    def __init__(self,filelog=False,console=False,module_name=None,decorate_methods=False):
+    def __init__(self,filelog=False,console=False,module_name=None):
          
         self.issue_url = "https://github.com/siddhant-curious/Python-Method-Execution-Time/issues"
         
@@ -36,10 +36,10 @@ class Timer:
             start_time = time.perf_counter()
             result = method(*args,**kwargs)
             end_time = time.perf_counter()
-            total_time = end_time-start_time
+            total_time = (end_time-start_time)*1000
             self.logtime_data[method.__name__]=total_time
             if self.console is True:
-                Timer.rootLogger.info(f'Time take by method : {method.__name__}  of {self.module_name} is {total_time}')
+                Timer.rootLogger.info(f'Time take by method : {method.__name__}  of {self.module_name} is {total_time} ms')
             return result
         return wrapper
     
@@ -56,8 +56,8 @@ class Timer:
     def auto_decorate(self):
         try:
             module = sys.modules[self.module_name]
-            methods = getmembers(module,isfunction)
-            for name,addr in methods:
+            items = getmembers(module,isfunction)
+            for name,addr in items:
                 setattr(module,name,self.timeit(addr))
         except KeyError as e:
             raise f'Error Occured, No module by name {module_name}. If you think this was a mistake than raise issue at {self.issue_url}'

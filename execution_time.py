c5bc7ec835e1d48ee7fcaf9f6279b40eca3426db
Siddhant Chhabra <siddhant1042@gmail.com>
2019-06-30 19:44:40 +0530

    Added Logs and Apply All

--- execution_time.py
+++ execution_time.py
@@ -1,22 +1,44 @@
 import functools
 import time
 import os
-import logger
-
-'''
-1. console logging
-2. logging file
-3. all methods, automatically
-'''
+import logging
+from inspect import getmembers,isfunction
+import sys
 
 
-class Timer():
-    def __init__(self,filelog=False,console=False,all_methods=False):
+class Timer:
+
+    logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+    rootLogger = logging.getLogger()
+    rootLogger.setLevel(logging.DEBUG)
+
+    def __init__(self,filelog=False,console=False,module_name=None):
+         
+        self.issue_url = "https://github.com/siddhant-curious/Python-Method-Execution-Time/issues"
+        
         self.filelog = filelog
+        if self.filelog is True:
+            fileHandler = logging.FileHandler("execution_times1.log")
+            fileHandler.setFormatter(Timer.logFormatter)
+            Timer.rootLogger.addHandler(fileHandler)
+        
         self.console = console
-        self.all_methods = all_methods
+        if self.console is True: 
+            consoleHandler = logging.StreamHandler(sys.stdout)
+            consoleHandler.setFormatter(Timer.logFormatter)
+            Timer.rootLogger.addHandler(consoleHandler)
+        
+        self.module_name = module_name
+        if self.module_name is not None:
+            try:
+                module = sys.modules[module_name]
+                methods = getmembers(module,isfunction)
+                for name,addr in methods:
+                    setattr(module,name,self.timeit(addr))
+            except KeyError as e:
+                raise f'Error Occured, No module by name {module_name}. If you think this was a mistake than raise issue at {self.issue_url}'
         self.logtime_data = {}
-    
+        
     def timeit(self,method):
         @functools.wraps(method)
         def wrapper(*args,**kwargs):
@@ -25,6 +47,7 @@ class Timer():
             end_time = time.perf_counter()
             total_time = end_time-start_time
             self.logtime_data[method.__name__]=total_time
+            if self.console is True:
+                Timer.rootLogger.info(f'Time take by method : {method.__name__}  of {self.module_name} is {total_time}')
             return result
         return wrapper
-
